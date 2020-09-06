b1f0321955b325f3dd1c08cafc25d9784e06247d
Joseph Perry <josephperry720@gmail.com>
2020-09-06 15:41:39 -0400

    created central quiz object

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-console.log("hello");
+var answerEl = document.getElementById("answers");
+
+quizData = [
+  {
+    question: "Commonly used data types DO NOT include:",
+    answers: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+    correctAnswer: "3. alerts",
+  },
+  {
+    question:
+      "The condition in an If / Else Statement is enclosed within ________.",
+    answers: [
+      "1. quotes",
+      "2. curly brackets",
+      "3. parenthesis",
+      "4. square brackets",
+    ],
+    correctAnswer: "3. parenthesis",
+  },
+  {
+    question: "Arrays in JavaScript can be used to store ________.",
+    answers: [
+      "1. numbers and strings",
+      "2. other Arrays",
+      "3. booleans",
+      "4. all of the above",
+    ],
+    correctAnswer: "4. all of the above",
+  },
+  {
+    question:
+      "String values must be enclosed within ________ when being assigned to variables.",
+    answers: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
+    correctAnswer: "4. parenthesis",
+  },
+  {
+    question:
+      "A very usefule tool used during development and debugging for printing content to the debugger is:",
+    answers: [
+      "1. JavaScript",
+      "2. terminal / bash",
+      "3. for loops",
+      "4. console.log",
+    ],
+    correctAnswer: "4. console.log",
+  },
+];
